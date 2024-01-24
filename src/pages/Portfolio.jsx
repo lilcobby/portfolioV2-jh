ed3d1bfe2dadb568cdf4a9b4df3d7e5e5919acc7
Jacob <jshairfield13@gmail.com>
2024-01-23 20:19:17 -0500

    working on styling now

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,61 @@
+import '../styles/Portfolio.css';
+
 export default function Portfolio() {
   return (
-    <div className = "container">
+    <div className="container">
       <h1>Portfolio Page</h1>
       <p>
-    this is where our projects will go. images with anchor links to the github projects/ the server hosted projects.
+        This is where our projects will go. Images with anchor links to the GitHub projects/ the server hosted projects.
       </p>
+      <div className="project">
+        <h3>project 1</h3>
+        <p>project description</p>
+        <a href="">
+          <img src="./src/assets/" alt="project image" />
+        </a>
+      </div>
+      <div className="project">
+        <h3>project 2</h3>
+        <p>project description</p>
+        <a href="">
+          <img src="./src/assets/" alt="project image" />
+        </a>
+      </div>
+      <div className="project">
+        <h3>project 3</h3>
+        <p>project description</p>
+        <a href="">
+          <img src="./src/assets/" alt="project image" />
+        </a>
+      </div>
+      <div className="project">
+        <h3>project 4</h3>
+        <p>project description</p>
+        <a href="">
+          <img src="./src/assets/" alt="project image" />
+        </a>
+      </div>
+      <div className="project">
+        <h3>project 5</h3>
+        <p>project description</p>
+        <a href="">
+          <img src="./src/assets/" alt="project image" />
+        </a>
+      </div>
+      <div className="project">
+        <h3>project 6</h3>
+        <p>project description</p>
+        <a href="">
+          <img src="./src/assets/" alt="project image" />
+        </a>
+      </div>
+      <div className="project">
+        <h3>project 1</h3>
+        <p>project description</p>
+        <a href="">
+          <img src="./src/assets/" alt="project image" />
+        </a>
+      </div>
     </div>
   );
 }
