fb2c41a11dc1c3894057f676055f5f2f7680a8f7
Jacob <jshairfield13@gmail.com>
2024-01-24 22:00:54 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
-import Head from "./pages/Header"
+import Head from "./pages/Header";
+import Foot from "./pages/Foot"
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
       <Nav />
       <main className="mx-3">
-        <h2>I'm going to style all of this when i'm ready to use it I dont have enought time to add my matrix terminal theme</h2>
+        <Head />
         <Outlet />
+        <Foot/>
       </main>
     </>
   );
--- src/pages/Foot.jsx
+++ src/pages/Foot.jsx
@@ -1,8 +1,8 @@
-export default function Head() {
+export default function Foot() {
     return (
       // className container is the same as class container so that built in js doesnt throw errors 
       <div className = "container">
-  <h1>I'm ok with this skeleton for now, I will add my Matrix theme terminal this weekend</h1>
+  <h1>Foot Template</h1>
       </div>
     );
   }
